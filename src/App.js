a7d77dea5a0d9a592462ee650f85bf59b41ec744
yasmin <yasminmartinsdebrito@gmail.com>
2022-06-11 11:39:28 -0300

    feat: criando estilização completa da aplicação, estruturando codigo e clean code, instalação da biblioteca react-icons para usar o icone de filter para simulação do botão

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./app.css"
+import { FaFilter }  from "react-icons/fa"
 import Bar from "./components/Bar";
 import Scatter from "./components/Scatter";
 
@@ -7,10 +8,23 @@ function App() {
 
 
   return (
+    <>
+    <header></header>
     <main>
-      <Bar/>
-      <Scatter/>
+      <div id="text">
+        <div>
+          <h1>Dashboard</h1>
+          <p>Desafio Técnico Frontend</p>
+        </div>
+        <button><FaFilter/><span>Filtrar</span></button>
+      </div>
+      <section>
+        <Bar/>
+        <Scatter/>
+      </section>
     </main>
+    </>
+    
   );
 }
 
